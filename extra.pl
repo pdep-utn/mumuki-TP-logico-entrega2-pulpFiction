d5c10728745327bcf312f847fad40566ed330841
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 12:30:01 -0400

    Mumuki Export on 2016-05-02 12:30:01 -0400

---
+++ extra.pl
@@ -0,0 +1,26 @@
+% Información base
+personaje(pumkin,     ladron([licorerias, estacionesDeServicio])).
+personaje(honeyBunny, ladron([licorerias, estacionesDeServicio])).
+personaje(vincent,    mafioso(maton)).
+personaje(jules,      mafioso(maton)).
+personaje(marsellus,  mafioso(capo)).
+personaje(winston,    mafioso(resuelveProblemas)).
+personaje(mia,        actriz([foxForceFive])).
+personaje(butch,      boxeador).
+personaje(bernardo,   mafioso(cerebro)).
+personaje(bianca,     actriz([elPadrino1])).
+
+% encargo(Solicitante, Encargado, Tarea). 
+% las tareas pueden ser cuidar(Protegido), ayudar(Ayudado), buscar(Buscado, Lugar)
+encargo(marsellus, vincent,   cuidar(mia)).
+encargo(vincent,  elVendedor, cuidar(mia)).
+encargo(marsellus, winston, ayudar(jules)).
+encargo(marsellus, winston, ayudar(vincent)).
+encargo(marsellus, vincent, buscar(butch, losAngeles)).
+encargo(bernardo, vincent, buscar(jules, fuerteApache)).
+encargo(bernardo, winston, buscar(jules, sanMartin)).
+encargo(bernardo,  winston, buscar(jules, lugano)).
+
+amigo(vincent, jules).
+amigo(jules, jimmie).
+amigo(vincent, elVendedor).
