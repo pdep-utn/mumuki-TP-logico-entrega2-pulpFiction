f0869eaabd60305000d01744bd9a3c705302db16
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 15:21:20 -0400

    Mumuki Export on 2016-05-02 15:21:20 -0400

--- 00003_Personajes respetables/test.pl
+++ 00003_Personajes respetables/test.pl
@@ -1,8 +1,14 @@
 test('Nivel de respeto de Mia es 1, porque hizo una película'):-
   nivelRespeto(mia, 0.1).
 
-test('Nivel de respeto de Butch es 0, porque es boxeador'):-
-  not(nivelRespeto(butch, _)).
+test('Nivel de respeto de Vincent es 15, porque es un matón y tiene 3 encargos'):-
+  nivelRespeto(vincent, 15).
+  
+test('Nivel de respeto de Marsellus es 20, porque es un mafioso capo'):-
+  nivelRespeto(vincent, 20).
+    
+test('Nivel de respeto de Winston es 10, porque es un mafioso que resuelve problemas'):-
+  nivelRespeto(vincent, 10).
   
 test('Marsellus, Vincent y Winston son respetables', set(Quien = [[marsellus, vincent, winston]] )):-
   personajesRespetables(Quien).
