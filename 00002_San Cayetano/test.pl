d82781c65b88653ecc5e050739c7c787d860fecd
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 14:07:31 -0400

    Mumuki Export on 2016-05-02 14:07:31 -0400

--- 00002_San Cayetano/test.pl
+++ 00002_San Cayetano/test.pl
@@ -2,5 +2,5 @@ test('Bernardo es San Cayetano', set(Quien = [bernardo] )):-
   sanCayetano(Quien).
 
 test('Ningún otro que no sea Bernardo es San Cayetano', set(Personaje =[bianca,butch,honeyBunny,jules,marsellus,mia,pumkin,vincent,winston])):-
-  personaje(Personaje, _), 
+  tieneCerca(Personaje, _), 
   not(sanCayetano(Personaje)).
