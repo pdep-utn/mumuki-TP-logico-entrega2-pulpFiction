716c2d65a1ffe1661cc73c7ab6dc717c9fd0168c
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 13:03:15 -0400

    Mumuki Export on 2016-05-02 13:03:15 -0400

--- 00001_Es peligroso/test.pl
+++ 00001_Es peligroso/test.pl
@@ -1,5 +1,5 @@
 test('Bernardo es pareja de Bianca y Charo', set(Quien = [vincent, jules, pumkin, honeyBunny, bernardo])):-
     esPeligroso(Quien).
     
-test('Bianca y Demóstenes no son peligrosos'):-set(Quien = [bianca, demostenes]),
+test('Bianca y Demóstenes no son peligrosos', set(Quien = [bianca, demostenes])):-
     not(esPeligroso(Quien)).
