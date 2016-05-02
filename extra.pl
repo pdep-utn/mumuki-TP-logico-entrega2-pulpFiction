86cc7dc4a73963dc377c13e1f5ddb6257aab7d87
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-05-02 13:01:11 -0400

    Mumuki Export on 2016-05-02 13:01:11 -0400

--- extra.pl
+++ extra.pl
@@ -32,3 +32,6 @@ trabajaPara(marsellus, winston).
 
 % Bernardo trabaja para cualquiera que trabaja para marsellus (salvo para jules)
 trabajaPara(Empleador, bernardo):-trabajaPara(marsellus, Empleador), Empleador \= jules.
+
+% George trabaja para todos los que salen con Bernardo
+trabajaPara(Empleador, george):-saleCon(Empleador, bernardo).
