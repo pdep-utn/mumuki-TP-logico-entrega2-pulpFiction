0f1e0ad7195fae3f2f9c2a10d21fe356502a0d5d
fernando.dodino@gmail.com <fernando.dodino@gmail.com>
2016-06-20 14:58:15 -0400

    Mumuki Export on 2016-06-20 14:58:15 -0400

--- 00005_Personajes respetables/test.pl
+++ 00005_Personajes respetables/test.pl
@@ -4,5 +4,5 @@ test('Los personajes respetables son 3 de 10'):-
 test('vincent, marsellus y winston son respetables', set(Quien = [vincent, marsellus, winston] )):-
   respetable(Quien).
 
-test('vincent, marsellus y winston no pertenecen al conjunto de no respetables', set(Quien = [vincent, marsellus, winston] )):-
+test('jules no es respetable', set(Quien = [pumkin, honeyBunny, jules, mia, butch, bernardo, bianca] )):-
   not(respetable(Quien)).
