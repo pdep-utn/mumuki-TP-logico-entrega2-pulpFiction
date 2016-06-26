bb3e25a4a4fc7af6c9118718233e332865ca8b2a
occhiuzzi.santiago@gmail.com <occhiuzzi.santiago@gmail.com>
2016-06-25 21:36:17 -0400

    Mumuki Export on 2016-06-25 21:36:17 -0400

--- 00005_Personajes respetables/test.pl
+++ 00005_Personajes respetables/test.pl
@@ -1,8 +1,8 @@
-test('Los personajes respetables son 3 de 13'):-
+test('Los personajes respetables son 3 de 13 (hay 10 no respetables)'):-
   respetabilidad(3,10).
   
 test('vincent, marsellus y winston son respetables', set(Quien = [vincent, marsellus, winston] )):-
   respetable(Quien).
 
-test('jules, pumpkin, honeybunny, mia, butch, elVendedor, jimmie, bernardo y bianca no son respetables', set(Quien = [pumkin, honeyBunny, jules, mia, butch, bernardo, bianca, elVendedor, jimmie] )):-
+test('pumkin, honeyBunny, jules, mia, butch, bernardo, bianca, elVendedor y jimmie no son respetables', set(Quien = [pumkin, honeyBunny, jules, mia, butch, bernardo, bianca, elVendedor, jimmie] )):-
   personaje(Quien, _), not(respetable(Quien)).
