83e929fb959650858372966973bc22c8c4ede4a7
gastonprietoutn@gmail.com <gastonprietoutn@gmail.com>
2016-07-06 12:17:34 -0400

    Mumuki Export on 2016-07-06 12:17:34 -0400

--- 00004_Más atareado/test.pl
+++ 00004_Más atareado/test.pl
@@ -7,5 +7,8 @@ test('El más atareado no es jules'):-
 test('El más atareado no es vincent'):-
   not(masAtareado(vincent)).
   
+test('El más atareado no es elVendedor'):-
+  not(masAtareado(elVendedor)).
+  
 test('cantidad de encargos de winston es 4'):-
   cantidadEncargos(winston,4).
